ecd84c41b440f19c1696b3b4af0629e6fceb3758
mrsptrn404 <marlinrespitarini@students.unnes.ac.id>
2025-05-22 09:09:43 +0700

    Create 1_dasboard.py

---
+++ pages/1_dasboard.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import joblib
+from.sklearn.model selection import train_test_split
+from.sklearn.matrics import classification_report, accuracy_score
+
+st.set_page_config(page_title="Predictions")
+st.tittle("Predictions")
+st.header("Predictions")
+
+#Load Dataset
