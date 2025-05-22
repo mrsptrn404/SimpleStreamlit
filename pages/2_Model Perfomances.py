d0405334eb984cd74c9556e96b76950914681aa7
mrsptrn404 <marlinrespitarini@students.unnes.ac.id>
2025-05-22 09:28:52 +0700

    Update 2_Model Perfomances.py

--- pages/2_Model Perfomances.py
+++ pages/2_Model Perfomances.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import pandas as pd
 import joblib
-from.sklearn.model selection import train_test_split
+from.sklearn.model_selection import train_test_split
 from.sklearn.matrics import classification_report, accuracy_score
 
-st.set_page_config(page_title="Predictions")
+st.set_page_config(page_title="Predictions", page_icon="🔍")
 st.tittle("Predictions")
 st.header("Predictions")
