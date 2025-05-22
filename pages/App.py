fdec9d0129f313ba170372cb97064a7060c25dcb
mrsptrn404 <marlinrespitarini@students.unnes.ac.id>
2025-05-22 09:16:40 +0700

    Create App.py

---
+++ pages/App.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+import pickle
+import joblib
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+st.set_page_config(page_title="Iris Dashboard App", layout="centered")
+st.sidebar.header("Dashboard")
